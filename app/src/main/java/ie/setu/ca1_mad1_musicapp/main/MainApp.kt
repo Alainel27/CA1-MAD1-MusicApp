42e6768813e3e206e4ba088aea4425d094dfc593
Alan Z <20102420@wit.ie>
2025-10-24 09:43:15 +0100

    added comments to the persistence code

--- app/src/main/java/ie/setu/ca1_mad1_musicapp/main/MainApp.kt
+++ app/src/main/java/ie/setu/ca1_mad1_musicapp/main/MainApp.kt
@@ -24,18 +24,33 @@ class MainApp : Application() {
 
     //I used AI to generate this code involving the JSON persistence
     //All the code below is created with the help of AI
+    //The json gets stored in the device apps storage
+    //How to access to
+    //Go to View -> Tools windows -> Device Explorer -> Chose the emulator
+    //In the Files navigate to data -> data -> ie.setu.ca1_mad1_musicapp -> files -> placemarks.json
 
+    //Declares the function kotlin function as savePlacemarks()
     fun savePlacemarks() {
+        //Gson is the library from Google for converting between Kotlin/Java and JSON and .toJson(placemarks) takes the list of placemarks abd converts it to a JSON string
         val jsonString = Gson().toJson(placemarks)
+        //points to the apps internal Storage directory
+        //creates the file named placemarks.json inside that directory, and .writeText writes the JSON string to that file
         File(filesDir, "placemarks.json").writeText(jsonString)
     }
 
+    //All the code below is created with the help of AI
     fun loadPlacemarks() {
+        //This line points to the file that was saved earlier
         val file = File(filesDir, "placemarks.json")
+        //This line checks if the file exists, if any files were created before to be loaded
         if(file.exists()) {
+            //This line creates a variable that represents the list, It is important because it without it wont be able to convert the GSON object into the placemarkModel and to put it in a array list
             val type = object : TypeToken<ArrayList<PlacemarkModel>>() {}.type
+            //This line reads all the JSON texts from the file and converts it into it back into the kotlin list
             val savedPlacemarks: ArrayList<PlacemarkModel> = Gson().fromJson(file.readText(), type)
+            //Removes anything already in the memory so no duplicate data is formed
             placemarks.clear()
+            //Updates the placemarks loaded from the JSON
             placemarks.addAll(savedPlacemarks)
         }
     }
